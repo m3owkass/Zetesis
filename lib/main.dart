0d5806ecdf5b567fa7c4892448b4745e69e3f076
Meowkas _ <20233025709@estudantes.ifpr.edu.br>
2026-06-08 11:58:41 -0300

    refactor: mover firebase_options para lib/config e remover do rastreamento git

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:zetesis/auth_gate.dart';
 import 'package:zetesis/config/supabase_config.dart';
-import 'package:zetesis/firebase_options.dart';
+import 'package:zetesis/config/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
